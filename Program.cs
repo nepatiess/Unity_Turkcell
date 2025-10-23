0ea5dc8d4e11bf7bf269baa92326580d4be03325
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2025-10-23 22:17:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class MerhabaUnity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Merhaba Unity!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
