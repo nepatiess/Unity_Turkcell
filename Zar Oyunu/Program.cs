c151d02d456ad3ec6d619a25e465eabba5b4ff64
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2025-10-24 19:36:29 +0300

    Add files via upload

---
+++ Zar Oyunu/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Zar_Oyunu;
+
+namespace zarOyunu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            tavlaZari TavlaZari1 = new tavlaZari(6);
+            tavlaZari TavlaZari2 = new tavlaZari(6);
+
+            int zar1Sonuc = TavlaZari1.ZarAt();
+            int zar2Sonuc = TavlaZari2.ZarAt();
+
+            Console.WriteLine("Zar 1: " + zar1Sonuc);
+            Console.WriteLine("Zar 2: " + zar2Sonuc);
+
+            if (zar1Sonuc > zar2Sonuc)
+            {
+                Console.WriteLine("1. zar kazandı!");
+            }
+            else if (zar1Sonuc < zar2Sonuc)
+            {
+                Console.WriteLine("2. zar kazandı!");
+            }
+            else
+            {
+                Console.WriteLine("Berabere!");
+            }
+
+        }
+    }
+}
---
+++ Zar Oyunu/tavlaZari.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zar_Oyunu
+{
+    internal class tavlaZari
+    {
+        /// <summary>
+        /// Zarın alabileceği maksimum değer değişkeni
+        /// </summary>
+        int maksimumZarDegeri;
+
+        Random random = new Random();
+
+        /// <summary>
+        /// Zarın alabileceği maksimum değeri giriniz.
+        /// </summary>
+        /// <param name="maksimumZarDegeri"></param>
+        public tavlaZari(int maksimumZarDegeri)
+        {
+            // değişken               
+            this.maksimumZarDegeri = maksimumZarDegeri;
+        }
+
+        public int ZarAt()
+        {
+            // Rastgele sayı üret.
+
+            return random.Next(1, maksimumZarDegeri + 1 );
+        }
+    
+    
+    
+    
+    
+    }
+}
